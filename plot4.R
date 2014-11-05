fdc838e4a2fc5fd3b4e09c3a6ef5eb6c50183b52
Hydrandi <dreaser@gmx.net>
2014-11-05 19:01:47 +0100

    changed plot4

--- plot4.R
+++ plot4.R
@@ -12,6 +12,7 @@ dat <- data[data$Date >= "2007-02-01" & data$Date < "2007-02-03",]
 dat$DateTime <- paste(dat$Date, dat$Time)
 dat$DateTime <- strptime(dat$DateTime, "%Y-%m-%d %H:%M:%S")
 
+png("plot4.png")
 par(mfrow= c(2,2))
 with(dat, {
         plot(DateTime, Global_active_power, type="l", xlab="", ylab = "Global Active Power")
@@ -20,10 +21,8 @@ with(dat, {
         points(DateTime, Sub_metering_1, col ="black", type= "l")
         points(DateTime, Sub_metering_2, type="l", col ="red")
         points(DateTime, Sub_metering_3, type="l", col ="blue")
-        legend("topright", lty= 1, bty= "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        legend("topright", cex=0.75, lty= 1, bty= "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime")
 })
 
-
-dev.copy(png,'plot4.png')
 dev.off()
